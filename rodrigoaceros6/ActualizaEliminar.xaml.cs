0d979cd9a24b800029e74da8c367e3b42f165879
Administrador <Administrador@SGDPN-DTIC-SP>
2023-07-04 03:47:33 -0500

    Agregar archivos de proyecto.

---
+++ rodrigoaceros6/ActualizaEliminar.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace rodrigoaceros6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ActualizaEliminar : ContentPage
+    {
+        public ActualizaEliminar(int codigo, string nombre, string apellido, int edad)
+        {
+            InitializeComponent();
+            txtCodigo.Text = codigo.ToString();
+            txtNombre.Text = nombre;
+            txtApellido.Text = apellido;
+            txtEdad.Text = edad.ToString();
+
+        }
+
+        private void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+                cliente.UploadValues("https://192.168.1.102/uisraels5/ws_uisrael/post.php?codigo=" + txtCodigo.Text + "&nombre=" + txtNombre.Text + "&apellido=" + txtApellido.Text + "&edad=" + txtEdad.Text, "PUT", parametros);
+                Navigation.PushAsync(new MainPage());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
+
+
+        }
+
+        private void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtCodigo.Text);
+
+                cliente.UploadValues("https://192.168.1.102/uisraels5/ws_uisrael/post.php" + txtCodigo.Text, "DELETE", parametros);
+                Navigation.PushAsync(new MainPage());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
+
+        }
+    }
+}
